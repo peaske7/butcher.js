4b19fdd8601a141221613f4487d38d4de14700ec
DrPoppyseed <peaske16180@gmail.com>
2022-05-28 17:02:58 +0900

    Add tests

---
+++ tests/index.test.ts
@@ -0,0 +1,60 @@
+import { fireEvent } from '@testing-library/dom'
+import { Butcher, butcher, listen } from '../src'
+
+let countTracker: Butcher<{
+  count: number
+}>
+let countSpan: HTMLSpanElement
+let incBtn: HTMLButtonElement
+
+beforeEach(() => {
+  document.body.innerHTML = `
+    <button id='incBtn'>Increase</button>
+    <p>Count: <span id='countSpan'></span></p>
+  `
+
+  countTracker = butcher({
+    name: 'countTracker',
+    meat: {
+      count: 0
+    }
+  })
+
+  countSpan = document.getElementById('countSpan')!
+  incBtn = document.getElementById('incBtn')! as HTMLButtonElement
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test('is state accessible', () => {
+  countSpan.innerHTML = `${countTracker.count}`
+  expect(countSpan.innerHTML).toBe('0')
+})
+
+test('is state interactive', () => {
+  // update state
+  countTracker.count = 2
+  countSpan.innerHTML = `${countTracker.count}`
+  expect(countSpan.innerHTML).toBe('2')
+})
+
+test('does listen callback react to state changes', () => {
+  countSpan.innerHTML = '0'
+
+  // listen to state change
+  listen(countTracker, 'count', () => {
+    countSpan.innerHTML = `${countTracker.count}`
+  })
+
+  // update count on button click
+  incBtn.addEventListener('click', () => {
+    countTracker.count += 1
+  })
+
+  // simulate button click
+  fireEvent.click(incBtn)
+
+  expect(countSpan.innerHTML).toBe('1')
+})
